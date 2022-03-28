b9484b5d7ddfdd7c349028c90d6716db44fa0b3d
rfabriciors <rfabriciors@gmail.com>
2022-03-28 20:10:58 -0300

    Added Ethereum and Kusama support

--- check-blockchain.sh
+++ check-blockchain.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#set -x
 
 hostname=$(hostname)
-bsc_scan_token="api_key"
+diff_acceptable=10
+slack_token="___________________"
+bsc_scan_token="___________________"
+eth_scan_token="___________________"
 
 # Matic test
 if [ ! -z $(echo $hostname|grep matic) ]; then
@@ -18,25 +22,28 @@ fi
 if [ ! -z $(echo $hostname|grep bsc) ]; then
     last_local_block=$(curl -k http://localhost:9137/api/v2|jq '.blockbook.bestHeight')
     remote_block=$(curl "https://api.bscscan.com/api?module=block&action=getblocknobytime&timestamp=$(date +%s)&closest=before&apikey=$bsc_scan_token"|jq '.result'|sed 's/\"//g')
+    network='BSC'
 fi
 
-# # ethereum test
-# if [ ! -z $(echo $hostname|grep bsc) ]; then
-#     actual_block=$(curl -k http://localhost:9137/api/v2|jq '.blockbook.bestHeight')
-# fi
+# ethereum test
+if [ ! -z $(echo $hostname|grep eth) ]; then
+    last_local_block=$(curl -k http://localhost:9136/api/v2|jq '.blockbook.bestHeight')
+    remote_block=$(curl -ks "https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=$(date +%s)&closest=before&apikey=$eth_scan_token"|jq '.result'|sed 's/\"//g')
+    network='ETH'
+fi
 
+# kusama test
+if [ ! -z $(echo $hostname|grep kusama) ]; then
+    last_local_block=$(curl -ks --request POST --url http://localhost:8082/v1/graphql --header 'Content-Type: application/json' --header 'x-hasura-admin-secret: klever' --data '{"query":"query GetBlocks {\n  block(limit: 1, order_by: {block_number: desc})  {\n    block_number\n    block_hash\n    total_extrinsics\n    finalized\n    timestamp\n  }\n}","operationName":"GetBlocks"}'|jq '.data.block[0].block_number')
+    remote_block=$(curl -sS -X POST -H "x-api-key: YOUR_KEY" https://kusama.api.subscan.io/api/scan/metadata|jq '.data.blockNum'|sed 's/\"//g')
+    network='KUSAMA'
+fi
 
-# printf %d $(curl -ks https://polygon-mainnet.g.alchemyapi.io/v2/demo \
-#         -X POST \
-#         -H "Content-Type: application/json" \
-#         -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":0}'|jq '.result'|sed 's/\"//g')
-
-
-echo $last_local_block $remote_block
+# echo $last_local_block $remote_block
 calc=$(($last_local_block-$remote_block))
 diff=$(echo ${calc#-})
 
  
-if [ $diff -gt 0 ]; then
-    curl -s -X POST --data-urlencode "payload={\"channel\": \"#blockchain-nodes-squad\", \"$network\": \"Matic\", \"text\": \"$network Node not synced ($hostname). The difference is $diff blocks\", \"icon_emoji\": \":thumbsdown:\"}" https://hooks.slack.com/services/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
+if [ $diff -gt $diff_acceptable ]; then
+    curl -s -X POST --data-urlencode "payload={\"channel\": \"#blockchain-nodes-squad\", \"$network\": \"$network\", \"text\": \"$network Node not synced ($hostname). The difference is $diff blocks\", \"icon_emoji\": \":thumbsdown:\"}" https://hooks.slack.com/services/$slack_token
 fi
